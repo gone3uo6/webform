61b4e45b30cef207065fdfc998bfde95b587b3be
jrockowitz <jrockowitz@gmail.com>
2017-02-14 17:58:57 -0500

    Issue #2838423 by bucefal91, jrockowitz: Drafts for anonymous users.

--- modules/webform_node/src/Tests/WebformNodeResultsTest.php
+++ modules/webform_node/src/Tests/WebformNodeResultsTest.php
@@ -5,6 +5,7 @@ namespace Drupal\webform_node\Tests;
 use Drupal\Core\Url;
 use Drupal\webform\Entity\Webform;
 use Drupal\webform\Tests\WebformTestBase;
+use Drupal\webform\WebformInterface;
 
 /**
  * Tests for webform node results.
@@ -112,7 +113,7 @@ class WebformNodeResultsTest extends WebformTestBase {
     $this->assertRaw('<a href="' . $node_submission_url->toString() . '" rel="next" title="Go to next page">Next submission <b>›</b></a>');
 
     // Check webform node saved draft.
-    $webform->setSetting('draft', TRUE);
+    $webform->setSetting('draft', WebformInterface::DRAFT_ENABLED_AUTHENTICATED);
     $webform->save();
 
     // Check webform saved draft.
--- src/Tests/WebformSubmissionStorageTest.php
+++ src/Tests/WebformSubmissionStorageTest.php
@@ -5,6 +5,7 @@ namespace Drupal\webform\Tests;
 use Drupal\Core\Serialization\Yaml;
 use Drupal\webform\Entity\Webform;
 use Drupal\webform\Entity\WebformSubmission;
+use Drupal\webform\WebformInterface;
 
 /**
  * Tests for webform storage tests.
@@ -94,7 +95,7 @@ class WebformSubmissionStorageTest extends WebformTestBase {
     $this->drupalLogout();
 
     // Enable the saving of drafts.
-    $webform->setSetting('draft', TRUE)->save();
+    $webform->setSetting('draft', WebformInterface::DRAFT_ENABLED_AUTHENTICATED)->save();
 
     // Create drafts for user1 and user2.
     $this->drupalLogin($user1);
